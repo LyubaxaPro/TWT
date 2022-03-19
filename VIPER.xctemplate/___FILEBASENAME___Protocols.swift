0651e0ec6df9a07841eba756ec8e919d0b7378f3
LyubaxaPro <lyubaxova@mail.ru>
2022-03-19 19:50:41 +0300

    #11 Реализовать более современный протокол

--- VIPER.xctemplate/___FILEBASENAME___Protocols.swift
+++ VIPER.xctemplate/___FILEBASENAME___Protocols.swift
@@ -12,20 +12,20 @@ protocol ___VARIABLE_ModuleName___ModuleInput {
 	var moduleOutput: ___VARIABLE_ModuleName___ModuleOutput? { get }
 }
 
-protocol ___VARIABLE_ModuleName___ModuleOutput: class {
+protocol ___VARIABLE_ModuleName___ModuleOutput: AnyObject {
 }
 
-protocol ___VARIABLE_ModuleName___ViewInput: class {
+protocol ___VARIABLE_ModuleName___ViewInput: AnyObject {
 }
 
-protocol ___VARIABLE_ModuleName___ViewOutput: class {
+protocol ___VARIABLE_ModuleName___ViewOutput: AnyObject {
 }
 
-protocol ___VARIABLE_ModuleName___InteractorInput: class {
+protocol ___VARIABLE_ModuleName___InteractorInput: AnyObject {
 }
 
-protocol ___VARIABLE_ModuleName___InteractorOutput: class {
+protocol ___VARIABLE_ModuleName___InteractorOutput: AnyObject {
 }
 
-protocol ___VARIABLE_ModuleName___RouterInput: class {
+protocol ___VARIABLE_ModuleName___RouterInput: AnyObject {
 }
