661d332098a07436e01d70515d8fda148025a106
Georgiy <61705983+SmartDuck9000@users.noreply.github.com>
2022-04-10 18:12:08 +0300

    Revert "Feature/#4 event tracker"

--- EventTracker/Poster/PosterPresenter.swift
+++ EventTracker/Poster/PosterPresenter.swift
@@ -1,100 +1,34 @@
+//
+//  PosterPresenter.swift
+//  EventTracker
+//
+//  Created by Liubov Prokhorova on 19.03.2022.
+//  
+//
+
 import Foundation
-import Firebase
 
 final class PosterPresenter {
-    weak var moduleOutput: PosterModuleOutput?
 	weak var view: PosterViewInput?
+    weak var moduleOutput: PosterModuleOutput?
+
 	private let router: PosterRouterInput
 	private let interactor: PosterInteractorInput
-    
-    private var serviceInfos: PosterServiceInfo = PosterServiceInfo(location:"msk", category: [])
-    
+
     init(router: PosterRouterInput, interactor: PosterInteractorInput) {
         self.router = router
         self.interactor = interactor
     }
-    
-    private (set) var postersViewModels: [PosterViewModel] = []
-    var searchPostersViewModels: [PosterViewModel] = []
-    
 }
 
 extension PosterPresenter: PosterModuleInput {
-    
 }
 
 extension PosterPresenter: PosterViewOutput {
-    
-    func didLoadView() {
-        Auth.auth().addStateDidChangeListener {[weak self] (auth, user)  in
-            if user != nil {
-                self?.interactor.getCityService()
-            } else {
-                self?.interactor.load(posters: self?.serviceInfos ?? PosterServiceInfo(location: "msk", category: []))
-                self?.moduleOutput?.setNewCity(cityService: "msk")
-            }
-        }
-    }
-    
-    func didPullRefresh() {
-        interactor.load(posters: serviceInfos)
-    }
-    
-    func didTapCell(poster: PosterViewModel) {
-        interactor.isInFavorites(poster: poster)
-    }
-    
-    func didTapFilter() {
-        router.openFilter(output: self, defaultCityService: serviceInfos.location)
-    }
 }
 
 extension PosterPresenter: PosterInteractorOutput {
-    func setCityService(cityService: String) {
-        self.serviceInfos.location = cityService
-        interactor.load(posters: self.serviceInfos)
-        moduleOutput?.setNewCity(cityService: cityService)
-    }
-    
-    func isInFavorites(poster: PosterViewModel) {
-        router.showPoster(with: poster, isInFavorites: true, output: self)
-    }
-    
-    func notInFavorites(poster: PosterViewModel) {
-        router.showPoster(with: poster, isInFavorites: false, output: self)
-    }
-    
-    func didLoad(posters: [PosterResults]) {
-        
-        let postersViewModels = posters.map { poster in
-
-            return PosterViewModelManager.shared.posterResultsToPosterViewModel(poster: poster)
-        }
-        self.postersViewModels = postersViewModels
-        self.view?.reloadData()
-        moduleOutput?.setNewPosters(posters: posters)
-    }
-    
-    func didReceive(error: Error) {
-        router.showAlertErrorMessage(with: "Неожиданная ошибка")
-    }
 }
 
-extension PosterPresenter: DetailModuleOutput {
-}
-
-extension PosterPresenter: FilterModuleOutput {
-    func setFilterValues(filterValues: FilterValues) {
-        interactor.load(posters: PosterServiceInfo(location: filterValues.location, category: filterValues.categories))
-        moduleOutput?.setNewCity(cityService: filterValues.location)
-        view?.startRefreshing()
-    }
-}
-
-extension PosterPresenter: UserProfileModuleOutput {
-    func update() {
-        self.serviceInfos = PosterServiceInfo(location:"msk", category: [])
-        didLoadView()
-        router.clearViewStack()
-    }
+extension PosterPresenter: ProfileModuleOutput {
 }
--- EventTracker/Poster/PosterViewController.swift
+++ EventTracker/Poster/PosterViewController.swift
@@ -1,133 +1,33 @@
+//
+//  PosterViewController.swift
+//  EventTracker
+//
+//  Created by Liubov Prokhorova on 19.03.2022.
+//  
+//
+
 import UIKit
-import PinLayout
 
 final class PosterViewController: UIViewController {
-    private let output: PosterViewOutput
-    private let tableView = UITableView()
-    private let searchController = UISearchController(searchResultsController: nil)
-    private var searchBarIsEmpty: Bool {
-        guard let text = searchController.searchBar.text else { return false}
-        return text.isEmpty
-    }
-    private var isFiltering: Bool {
-        return searchController.isActive && !searchBarIsEmpty
-    }
+	private let output: PosterViewOutput
 
     init(output: PosterViewOutput) {
         self.output = output
+
         super.init(nibName: nil, bundle: nil)
     }
-    
-    required init?(coder: NSCoder) {
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = "Афиша"
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.separatorStyle = .none
-        
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(didPullRefresh), for: .valueChanged)
-        tableView.refreshControl = refreshControl
 
-        
-        tableView.register(EntertaimentTableViewCell.self, forCellReuseIdentifier: "EntertaimentTableViewCell")
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-        searchController.searchResultsUpdater = self
-        // взаимодействие с отфильтрованным
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Поиск"
-        navigationItem.searchController = searchController
-        //отпустить строку поиска при переходе на другой экран
-        definesPresentationContext = true
-        view.addSubview(tableView)
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage.init(systemName: "slider.horizontal.3"),
-                                                            style: .plain,
-                                                            target: self,
-                                                            action: #selector(didTapFilter))
-        output.didLoadView()
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        tableView.pin.all()
-    }
-    
-    @objc
-    private func didPullRefresh() {
-        output.didPullRefresh()
-    }
-    
-    @objc
-    private func didTapFilter() {
-        output.didTapFilter()
-    }
+        view.backgroundColor = .green
+	}
 }
-
-extension PosterViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if isFiltering {
-            return output.searchPostersViewModels.count
-        }
-        return output.postersViewModels.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "EntertaimentTableViewCell", for: indexPath) as? EntertaimentTableViewCell else {
-            return .init()
-            }
-
-        cell.layer.cornerRadius = 10
-        
-        if isFiltering {
-            cell.configure(with: output.searchPostersViewModels[indexPath.row])
-        } else {
-            cell.configure(with: output.postersViewModels[indexPath.row])
-        }
-        cell.selectionStyle = .none
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 136
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if isFiltering{
-            output.didTapCell(poster: output.searchPostersViewModels[indexPath.row])
-        } else {
-            output.didTapCell(poster: output.postersViewModels[indexPath.row])
-        }
-    }
-}
-
 
 extension PosterViewController: PosterViewInput {
-    func reloadData() {
-        self.tableView.refreshControl?.endRefreshing()
-        navigationItem.searchController = searchController
-        self.tableView.reloadData()
-    }
-
-    func startRefreshing() {
-        self.tableView.refreshControl?.beginRefreshing()
-    }
-}
-
-
-extension PosterViewController: UISearchResultsUpdating{
-    func updateSearchResults(for searchController: UISearchController) {
-        filterContentForSearch(searchController.searchBar.text ?? "")
-    }
-    
-    private func filterContentForSearch(_ searchText: String) {
-        output.searchPostersViewModels = output.postersViewModels.filter({ (poster: PosterViewModel) -> Bool in
-            (poster.short_title.lowercased().contains(searchText.lowercased()) || poster.title.lowercased().contains(searchText.lowercased()))
-        })
-        tableView.reloadData()
-    }
 }
